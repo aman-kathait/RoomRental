5b6716649a8c134acd6caae4f5fe820d4c79410e
Aman Kathait <aman.kathat011@gmail.com>
2025-11-29 12:48:45 +0530

    User Model done with password encryption plus adding jwt logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,10 @@ import cors from "cors";
 
 const app = express();
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
@@ -12,9 +16,9 @@ app.use(
   }),
 );
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to Room Rental Application");
