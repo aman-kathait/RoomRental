8b5cca84435637753590029d15a58dda3d61dab2
Aman Kathait <aman.kathat011@gmail.com>
2025-12-04 12:45:11 +0530

    Adding more controllers and routes from room

--- backend/src/controllers/room.controller.js
+++ backend/src/controllers/room.controller.js
@@ -1,39 +1,81 @@
-import { ApiResponse } from "../utils/api-response.js"
-import { ApiError } from "../utils/api-error.js"
-import { asyncHandler } from "../utils/async-handler.js"
-import Room from "../models/room.models.js"
-import User from "../models/user.models.js"
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import Room from "../models/room.models.js";
+import User from "../models/user.models.js";
 
+export const addRoom = asyncHandler(async (req, res) => {
+  const { propertyName, description, price, address, images, amenities } =
+    req.body;
 
-export const addRoom=asyncHandler(async(req,res)=>{
-    const {propertyName,description,price,address,images,amenities}=req.body;
+  if (!propertyName || !description || !price || !address) {
+    throw new ApiError(400, "Missing required fields");
+  }
 
-     if (!propertyName || !description || !price || !address) {
-        throw new ApiError(400, "Missing required fields");
+  if (!address.city || !address.pincode) {
+    throw new ApiError(400, "City and pincode are required in the address");
+  }
+
+  if (!images || !Array.isArray(images) || images.length === 0) {
+    throw new ApiError(400, "At least one image is required");
+  }
+
+  const ownerId = req.user?._id;
+
+  if (!ownerId) {
+    throw new ApiError(404, "Owner user not found");
+  }
+  const room = await Room.create({
+    owner: ownerId,
+    propertyName,
+    description,
+    price,
+    address,
+    images,
+    amenities,
+  });
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, { room }, "Room added successfully"));
+});
+
+export const getAllRooms = asyncHandler(
+  asyncHandler(async (req, res) => {
+    const rooms = await Room.find().populate(
+      "owner",
+      "name email fullName contactNumber",
+    );
+    if (!rooms || rooms.length === 0) {
+      throw new ApiError(404, "No rooms found");
     }
+    res
+      .status(200)
+      .json(new ApiResponse(200, { rooms }, "Rooms fetched successfully"));
+  }),
+);
 
-    if (!address.city || !address.pincode) {
-        throw new ApiError(400, "City and pincode are required in the address");
+export const getMyRooms = asyncHandler(async (req, res) => {
+  const ownerId = req.user?._id;
+  const myrooms = await Room.find({ owner: ownerId });
+  if (!myrooms || myrooms.length === 0) {
+    throw new ApiError(404, "No rooms found for this owner");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, { myrooms }, "My Rooms Fetched successfully"));
+});
+
+export const findRoomById=asyncHandler(async(req,res)=>{
+    const roomId=req.params.id;
+    const room=await Room.findById(roomId).populate(
+      "owner",
+      "name email fullName contactNumber",
+    );
+    if(!room){
+        throw new ApiError(404, "Room not found");
     }
-
-    if (!images || !Array.isArray(images) || images.length === 0) {
-        throw new ApiError(400, "At least one image is required");
-    }
-
-    const ownerId = req.user?._id;
-    
-    if(!ownerId){
-        throw new ApiError(404,"Owner user not found");
-    }
-    const room=await Room.create({
-        owner: ownerId,
-        propertyName,
-        description,
-        price,
-        address,
-        images,
-        amenities
-    });
-
-    res.status(201).json(new ApiResponse(201,{room},"Room added successfully"));
+    res
+    .status(200)
+    .json(new ApiResponse(200, { room }, "Room Fetched successfully"));
 });
--- backend/src/routes/room.routes.js
+++ backend/src/routes/room.routes.js
@@ -1,9 +1,12 @@
 import Router from 'express';
 import {verifyJWT,isLandlord} from '../middlewares/auth.middleware.js';
-import {addRoom} from '../controllers/room.controller.js';
+import {addRoom,getAllRooms, getMyRooms,findRoomById} from '../controllers/room.controller.js';
 
 const router=Router();
 
 router.route("/add-room").post(verifyJWT, isLandlord, addRoom);
+router.route("/get-all-rooms").get(verifyJWT, getAllRooms);
+router.route("/get-my-rooms").get(verifyJWT, getMyRooms);
+router.route("/get-room/:id").get(verifyJWT, findRoomById);
 
 export default router;
