fb82b8ded17da6dea891141383a4ace63f284184
Aman Kathait <aman.kathat011@gmail.com>
2025-12-07 12:23:31 +0530

    More Api added to inquiryModel

--- backend/src/controllers/inquiry.controller.js
+++ backend/src/controllers/inquiry.controller.js
@@ -28,7 +28,20 @@ export const contact=asyncHandler(async(req,res)=>{
     res.status(201).json(new ApiResponse(201,{inquiry},"Inquiry sent successfully"));
 });
 
-export const removeContact=asyncHandler(async(req,res)=>{});
+export const removeContact=asyncHandler(async(req,res)=>{
+    const userId=req.user._id;
+    const {inquiryId}=req.body;
+    const inquiry=await RoomInquiry.findById(inquiryId);
+
+    if(!inquiry){
+        throw new ApiError(404,"Inquiry not found");
+    }
+    if(inquiry.user.toString()!==userId.toString()){
+        throw new ApiError(403,"You are not authorized to delete this inquiry");
+    }
+    await RoomInquiry.findByIdAndDelete(inquiryId);
+    res.status(200).json(new ApiResponse(200,null,"Inquiry deleted successfully"));
+});
 
 export const getMyInquiriesLandlord=asyncHandler(async(req,res)=>{
     const userId=req.user._id;
@@ -42,4 +55,36 @@ export const getMyInquiriesLandlord=asyncHandler(async(req,res)=>{
     res.status(200).json(new ApiResponse(200,{inquiries},"Inquiries fetched successfully"));
 });
 
-export const getMyInquiriesTenant=asyncHandler(async(req,res)=>{});
+export const getMyInquiriesTenant=asyncHandler(async(req,res)=>{
+    const userId=req.user._id;
+    const inquiries=await RoomInquiry.find({user:userId}).populate('room', 'propertyName ');
+
+    if(!inquiries){
+        throw new ApiError(404,"You have no inquiries");
+    }
+
+    res.status(200).json(new ApiResponse(200,{inquiries},"Inquiries fetched successfully"));
+});
+
+export const updateInquiryStatus=asyncHandler(async(req,res)=>{
+    const userId=req.user._id;
+    const {inquiryId,status}=req.body;
+    const inquiry=await RoomInquiry.findById(inquiryId);
+
+    const validStatuses = ["pending", "confirmed", "rejected", "cancelled"];
+    if (!validStatuses.includes(status)) {
+        throw new ApiError(400, "Invalid status value");
+    }
+
+    if(!inquiry){
+        throw new ApiError(404,"Inquiry not found");
+    }
+    if(inquiry.owner.toString()!==userId.toString()){
+        throw new ApiError(403,"You are not authorized to update this inquiry");
+    }
+    const updatedInquiry=await RoomInquiry.findByIdAndUpdate(inquiryId, {$set:{status}}, {new:true});
+    if (!updatedInquiry) {
+        throw new ApiError(404, "Inquiry not found");
+    }
+     res.status(200).json(new ApiResponse(200, updatedInquiry, "Inquiry status updated successfully"));
+});
--- backend/src/routes/inquiry.routes.js
+++ backend/src/routes/inquiry.routes.js
@@ -1,9 +1,12 @@
 import {Router} from 'express';
-import { contact, getMyInquiriesLandlord } from '../controllers/inquiry.controller.js';
+import { contact, getMyInquiriesLandlord, removeContact, getMyInquiriesTenant, updateInquiryStatus} from '../controllers/inquiry.controller.js';
 import {verifyJWT,isLandlord,isTenant} from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router.route("/contact").post(verifyJWT, isTenant, contact);
 router.route("/myinquiries").get(verifyJWT, isLandlord, getMyInquiriesLandlord);
+router.route("/removecontact").delete(verifyJWT, isTenant, removeContact);
+router.route("/myinquiries/tenant").get(verifyJWT, isTenant, getMyInquiriesTenant);
+router.route("/update-inquiry-status").patch(verifyJWT, isLandlord, updateInquiryStatus);
 export default router;
