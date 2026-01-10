6aff6d3be7ae098230cc49f092233dab3ab060c0
Aman Kathait <aman.kathat011@gmail.com>
2026-01-10 19:43:54 +0530

    Revert "Production Chnages-2"
    This reverts commit 09d0f6dace253be98fc334ce3bb5c52b210617d1.

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 app.use(cookieParser())
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || ["http://localhost:5173"],
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173" || "https://roomrental-b63v.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
