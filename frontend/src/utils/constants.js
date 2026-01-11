de8e5fab6c0c02b662a1fa3b9616f832f2fca8e2
Aman Kathait <aman.kathat011@gmail.com>
2026-01-11 11:44:08 +0530

    Url Changed

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL="http://localhost:8000"
+export const BASE_URL="https://renteasy-svjj.onrender.com"
 export const USER_API_END_POINT = `${BASE_URL}/api/v1/auth`;
 export const ROOM_END_POINT_USER=`${BASE_URL}/api/v1/rooms`;
 export const ROOM_END_POINT_ADMIN=`${BASE_URL}/api/v1/rooms`;
